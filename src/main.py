551befaee892e7c7c9ecc528c8d5c7fd27152aa0
suvanbanerjee <banerjeesuvan@gmail.com>
2024-05-04 18:53:29 +0530

    check for '-' in stock name

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/{company_name}")
 def read_item(company_name: str):
-    pattern = re.compile(r"\b[A-Z]{1,5}\.?[A-Z]{1,3}\((NASDAQ|NYSE)\)")
+    pattern = re.compile(r"\b([A-Z]{1,5}\.?\-?[A-Z]{1,3})\((NASDAQ|NYSE)\)")
 
     url = "https://www.google.com/search?q=" + "symbol of " + company_name + " stock"
     html = requests.get(url).content
@@ -19,8 +19,8 @@ def read_item(company_name: str):
     result = pattern.search(stock_symbol)
 
     if result:
-        result = result.group()
-        result = result.split("(")
-        return {"stock_symbol": result[0]}
+        result = result.group(1)
+        print(result)
+        return {"stock_symbol": result}
     else:
         return {"stock_symbol": "Not found"}
--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-company_name = "jpmc"
-pattern = re.compile(r"\b[A-Z]{1,5}\.?[A-Z]{1,3}\((NASDAQ|NYSE)\)")
+company_name = "jhonsons"
+pattern = re.compile(r"\b([A-Z]{1,5}\.?\-?[A-Z]{1,3})\((NASDAQ|NYSE)\)")
 
 url = "https://www.google.com/search?q=" + "symbol of " + company_name + " stock"
 html = requests.get(url).content
@@ -15,9 +15,4 @@ stock_symbol = soup.text
 
 result = pattern.search(stock_symbol)
 
-if result:
-    result = result.group()
-    result = result.split("(")
-    print(result[0])
-else:
-    print("Not found")
+print(result.group(1))
